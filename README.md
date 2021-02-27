de8ac34a1e1ca5cd9a762e6aa90f50d4edfe8d29
jwchan <741755613@qq.com>
2021-02-27 12:13:06 +0800

    feat: 步骤条组件

---
+++ README.md
@@ -0,0 +1,15 @@
+## 全局依赖安装
+
+```bash
+yarn global add @vue/cli-service-global
+```
+
+用于快速预览组件，使用 vue serve 命令
+
+## 便捷依赖安装
+
+```bash
+vue add element
+```
+
+进入命令行交互，选择按需导入，然后中文。
---
+++ src/Steps.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tf-steps">
+    <div class="tf-steps-line"></div>
+    <div
+      class="tf-step"
+      v-for="index in count"
+      :key="index"
+      :style="{ color: active >= index ? activeColor : defaultColor }"
+    >
+      {{ index }}
+    </div>
+  </div>
+</template>
+
+<script>
+import './steps.css'
+export default {
+  name: 'TfSteps',
+  props: {
+    count: {
+      type: Number,
+      default: 3
+    },
+    active: {
+      type: Number,
+      default: 0
+    },
+    activeColor: {
+      type: String,
+      default: 'red'
+    },
+    defaultColor: {
+      type: String,
+      default: 'green'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
